3de98387b16bf6a65816aa227c6156e69e194118
Adeel <adeelahmad043@gmail.com>
2019-11-07 22:29:41 -0800

    Assignment

---
+++ assignment_1.py
@@ -0,0 +1,14 @@
+'''
+1. Write a Python program to print the following string in a specific format
+(see the output).
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are
+'''
+print("\t\tTwinkle, twinkle, little star")
+print("\t\t\tHow I wonder what you are!")
+print("\t\t\t\tUp above the world so high\n\t\t\t\tLike a dimond in the sky")
+print("\t\tTwinkle, twinkle, little star\n\t\t\tHow I wonder what you are!")
